952f33cfc8a44c39be608998ef2d3cdb0fd9248a
Yash <yash140498@gmail.com>
2025-03-28 13:47:05 +0530

    added ENV keys to used by agent

--- server/agent/agent.js
+++ server/agent/agent.js
@@ -4,22 +4,49 @@ import { tool } from "@langchain/core/tools";
 import { MemorySaver } from "@langchain/langgraph";
 import { z } from "zod";
 
-const weatherTool = tool(
-  async (location) => {
-    console.log(location);
-    // TODO: Implement weather API call
+async function evalAndCaptureOutput(code) {
+  const oldLog = console.log;
+  const oldError = console.error;
 
-    return "The weather in " + location + " is sunny";
+  const output = [];
+  let errorOutput = [];
+
+  console.log = (...args) => output.push(args.join(" "));
+  console.error = (...args) => errorOutput.push(args.join(" "));
+
+  try {
+    await eval(code);
+    console.log(await eval(code));
+  } catch (error) {
+    errorOutput.push(error.message);
+  }
+
+  console.log = oldLog;
+  console.error = oldError;
+
+  return { stdout: output.join("\n"), stderr: errorOutput.join("\n") };
+}
+const jsExecutor = tool(
+  async ({ code }) => {
+    console.log('Running js code:', code);
+    const result = await eval(code);
+    return result;
   },
   {
-    name: "weather",
-    description: "Get the weather in a given location",
+    name: 'run_javascript_code_tool',
+    description: `
+      Run general purpose javascript code. 
+      This can be used to access Internet or do any computation that you need. 
+      The output will be composed of the stdout and stderr. 
+      The code should be written in a way that it can be executed with javascript eval in node environment.
+      you have following API keys at disposal:
+      WEATHER_API_KEY: process.env.OPEN_WEATHER_API_KEY
+    `,
     schema: z.object({
-      location: z.string().describe("The location to get the weather for"),
+      code: z.string().describe('code to be executed'),
     }),
   }
 );
-
 const model = new ChatOpenAI({
   modelName: "gpt-3.5-turbo",
   apiKey: process.env.OPENAI_API_KEY,
@@ -28,6 +55,6 @@ const checkpointSaver = new MemorySaver();
 
 export const agent = createReactAgent({
   llm: model,
-  tools: [weatherTool],
+  tools: [jsExecutor],
   checkpointSaver,
 });
